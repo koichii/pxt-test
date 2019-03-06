6a7447ea99c87af675a2bd661d11ff9152efc0b8
Koichi <koichii@live.jp>
2019-03-06 12:53:05 +0900

    first update

--- main.ts
+++ main.ts
@@ -5,3 +5,10 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `);
+
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
+namespace comment {
+    //% blockId=show_strings block="コメント %v"
+    export function noaction(text: string): void {
+    }
+}
