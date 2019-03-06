511bea11fce0ca9005d9664b7cb3924dc665aa42
Koichi <koichii@live.jp>
2019-03-06 13:05:01 +0900

    Updates.

--- test.ts
+++ test.ts
@@ -1,5 +1,4 @@
 // tests go here; this will not be compiled when this package is used as a library
-// tests go here; this will not be compiled when this package is used as a library
 basic.forever(() => {
-    tropic.pick(0)
+    comment.noaction("test")
 })
